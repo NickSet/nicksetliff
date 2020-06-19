924eb248415bb4deb937066671e3b27d1d1c7f07
Nick Setliff <nicksetliff@gmail.com>
2020-06-18 20:57:57 -0400

    Update vapor; change navBar syntax to work with leaf

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc.1.1"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc.3.1"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .target(name: "App", dependencies: [
